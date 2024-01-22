2abe133afb0a9af822f7d8d2a58b8f8599033ea0
pylaptop <462865454@qq.com>
2024-01-23 00:49:47 +0800

    测试

--- src/Demo.java
+++ src/Demo.java
@@ -3,5 +3,6 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello World");//测试笔记本git1
+        System.out.println("Hello World");//测试笔记本git测试
     }
 }
